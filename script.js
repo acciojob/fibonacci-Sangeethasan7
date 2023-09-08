6229f8f1ca2690920778c19342967ce6bb1938d2
Sangeethasan7 <120364555+Sangeethasan7@users.noreply.github.com>
2023-09-08 12:34:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let fib = [0, 1];
+    for (let i = 2; i <= num; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[num];
+  }
 }
 
-module.exports = fibonacci;
+// Example usage:
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
